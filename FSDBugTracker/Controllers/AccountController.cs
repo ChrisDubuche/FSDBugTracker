ccb47b629f7b3b9c470f2635fe9cb13d3f4fee22
Christian Yves Dubuche <dubuche@gmail.com>
2017-08-31 11:39:20 -0400

    Updated SignIn & Log Off actions & User Role Management

--- FSDBugTracker/Controllers/AccountController.cs
+++ FSDBugTracker/Controllers/AccountController.cs
@@ -396,7 +396,7 @@ namespace FSDBugTracker.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
